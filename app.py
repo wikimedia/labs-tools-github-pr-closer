ae23aa9abe0bb98e8deb4d588b32f40c1cb721d4
Taavi Väänänen <hi@taavi.wtf>
2023-06-20 16:15:05 +0300

    remove broken git version stuff
    Change-Id: I20b5a6b54886b599d64b4fa0b7531febb01a23ff

--- app.py
+++ app.py
@@ -1,33 +1,8 @@
-import subprocess
 from flask import Flask, render_template
 from webhook_handler import webhook
 
-
-def get_revision():
-    try:
-        output = (
-            subprocess.check_output(
-                ["git", "describe", "--always"], stderr=subprocess.STDOUT
-            )
-            .strip()
-            .decode()
-        )
-        assert "fatal" not in output
-        return output
-    except Exception:
-        # if somehow git version retrieving command failed, just return
-        return ""
-
-
 app = Flask(__name__)
 app.register_blueprint(webhook)
-
-
-@app.context_processor
-def inject_base_variables():
-    return {
-        "revision": get_revision(),
-    }
 
 
 @app.route("/")
