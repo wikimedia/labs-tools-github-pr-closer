cbaa88797c90a34d8fb245445f48246756aaed39
Taavi Väänänen <hi@taavi.wtf>
2023-06-20 16:38:38 +0300

    fix envvar usage a bit more
    Change-Id: I1668509bcdd8d776bdc8a0010e0129746fb72208

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ app.register_blueprint(webhook)
 
 @app.route("/")
 def index():
-    return render_template(
-        "index.html", {"app_id": os.environ.get("GITHUB_APP_ID_ENVVAR")}
-    )
+    return render_template("index.html", app_id=os.environ.get("GITHUB_APP_ID_ENVVAR"))
 
 
 if __name__ == "__main__":
